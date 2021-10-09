9e4d7e1ed2e2045052d1bad107bfcd223cdc6c0c
CheeseCheesson <kira7999@gmail.com>
2021-10-09 10:40:44 +0300

    add chanding button color

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,17 @@
 let links = document.querySelectorAll('a')
-
+let btns = document.querySelectorAll('.btn')
 links.forEach((link) => {
     link.addEventListener('click', (event) => {
         event.preventDefault()
     })
 })
+
+console.log(btns);
+
+btns.forEach(btn => {
+    function changeColor(){
+        btn.classList.toggle('_change_btn_color')
+    }
+    setInterval(changeColor, 1500)
+})
+
